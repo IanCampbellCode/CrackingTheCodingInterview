d6ee8efe5167305c514eac5ec23b68964a6eacfd
Ian Campbell <grimerphd@gmail.com>
2017-02-07 08:23:10 -0500

    Adds second folder hierachy within java for chapter 2 question 2

--- java/Chapter01/Question1.01/isUnique.java
+++ java/Chapter01/Question1.01/isUnique.java
@@ -1,12 +1,12 @@
 package IsUnique;
+import java.util.Scanner;
 
 public class IsUnique {
 
 	//Checks if every character in the string is unique
 	//Assumes ASCI alphabet, which only has 128 characters
 	public static boolean isUnique(String str) {
-
-		//If the characters are asci there are 128 possible characters
+		//If the characters are asci there are 128 possible unique characters
 		//Then by the pigeon hole principle there must be a recurring character if there are over 128 characters
 		if(str.length() > 128) {
 			return false;
@@ -14,6 +14,7 @@ public class IsUnique {
 	}
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner sc(System.in);
 
 	}
 }
