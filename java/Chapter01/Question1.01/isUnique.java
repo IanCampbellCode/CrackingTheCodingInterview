7e58a61d000fb75da4d0a87dee094e6a583a88b8
Ian Campbell <grimerphd@gmail.com>
2017-02-06 22:29:31 -0500

    Creates Java Directory and Chapter 1, Question 1 in it
    This adds a new file hiearchy which lays the ground work for
    future folders. From here on out, each language gets its own
    folder. Within each language there will be a folder for each
    chapter. Within each chapter there will be a folder for
    each question. Each folder contains a text file which contains
    the question and a language appropriate solution to the
    problem.

---
+++ java/Chapter01/Question1.01/isUnique.java
@@ -0,0 +1,20 @@
+package IsUnique;
+
+public class IsUnique {
+
+	//Checks if every character in the string is unique
+	//Assumes ASCI alphabet, which only has 128 characters
+	public static boolean isUnique(String str) {
+
+		//If the characters are asci there are 128 possible characters
+		//Then by the pigeon hole principle there must be a recurring character if there are over 128 characters
+		if(str.length() > 128) {
+			return false;
+		}
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
+
